a3bd8c524ee6a90c5230660018664b3b65fcd443
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2022-04-20 17:16:25 -0300

    a

--- components/SecaoMenu.tsx
+++ components/SecaoMenu.tsx
@@ -12,6 +12,6 @@ interface SecaoMenuProps {
 export default function SecaoMenu(props: SecaoMenuProps) {
 
     return (
-        <div className={`text-3xl text-casamento-400 p-2 flex justify-start  w-fit h-12`} onClick={props.onClick}>{props.texto} <span className={`w-30 h-full pt-1 ${props.paginasVisivel ? 'rotate-0 transition duration-500' : 'rotate-180 duration-500'}`}><Image src={arrow} alt='Seta' layout='fixed' objectFit='cover' objectPosition={'center'}/></span></div>
+        <div className={`text-3xl text-casamento-400 p-2 flex justify-start  w-fit h-12`} onClick={props.onClick}>{props.texto} <span className={`w-30 h-full pt-1 ${props.paginasVisivel || props.listaPresentesVisivel || props.confirmarPresencaVisivel ? 'rotate-0 transition duration-500' : 'rotate-180 duration-500'}`}><Image src={arrow} alt='Seta' layout='fixed' objectFit='cover' objectPosition={'center'}/></span></div>
     )
 }
