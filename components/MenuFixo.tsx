2390df838f2ba93b3f382e738f77737f40afe3d7
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2022-04-20 15:43:26 -0300

    Ajuste padding menu fixo

--- components/MenuFixo.tsx
+++ components/MenuFixo.tsx
@@ -17,8 +17,8 @@ export default function MenuFixo(props: MenuFixoProps) {
     const {menuShow, setMenuShow} = useMenuShow()
     return (
         <nav className={`fixed top-0 flex justify-between h-14 w-screen ${props.transparente ? 'bg-transparent transition ease-in duration-100' : 'bg-casamento-900 border-b border-[#9b6c14] transition ease-out duration-100'} z-40`}>
-            <div className={`p-3 font-bold ${props.transparente ? 'text-white' : 'text-black'}`}>{props.letrasIniciais}</div>
-            <div className={`flex flex-wrap justify-center items-center w-10 h-9 p-2 mt-1`} onClick={props.onClick}>
+            <div className={`p-4 font-bold ${props.transparente ? 'text-white' : 'text-black'}`}>{props.letrasIniciais}</div>
+            <div className={`flex flex-wrap justify-center items-center w-10 h-9 p-2 mt-2`} onClick={props.onClick}>
                 {props.menu ? (
                     <>
                         <div>
