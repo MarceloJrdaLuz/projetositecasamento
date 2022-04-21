5d0f085bebd79a8d3e205ea0a79dbb2ccf813f5e
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2022-04-21 00:20:58 -0300

    Link para paginal inicial

--- components/DataCasamento.tsx
+++ components/DataCasamento.tsx
@@ -6,7 +6,7 @@ interface DataCasamentoProps{
 export default function DataCasamento(props: DataCasamentoProps){
     return(
         <div className={`flex justify-center items-center relative`}>
-            <div className={`text-[26px] font-['Ultra'] text-white font-extrabold tracking-widest`}>
+            <div className={`text-[26px] font-['Ultra'] text-casamento-900 font-extrabold tracking-widest`}>
                 {props.data}
             </div>
             <div className={`bg-transparent absolute text-2xl text-[#9b6c14] tracking-wider`}>
--- components/MenuFixo.tsx
+++ components/MenuFixo.tsx
@@ -5,30 +5,33 @@ import hamburguer from '../public/hamburguer.svg'
 import cancelBlack from '../public/cancelblack.svg'
 import hamburguerBlack from '../public/hamburguerblack.svg'
 import 'next/image'
+import Link from "next/link"
 
 interface MenuFixoProps {
     letrasIniciais: string
     transparente: boolean
     menu?: boolean
-    onClick: ()=>void
+    onClick: () => void
 }
 
 export default function MenuFixo(props: MenuFixoProps) {
-    const {menuShow, setMenuShow} = useMenuShow()
+    const { menuShow, setMenuShow } = useMenuShow()
     return (
         <nav className={`fixed top-0 flex justify-between h-14 w-screen ${props.transparente ? 'bg-transparent transition duration-1000' : 'bg-casamento-900 border-b border-[#9b6c14] transition duration-500'} z-40`}>
-            <div className={`p-4 font-bold ${props.transparente ? 'text-white' : 'text-black'}`}>{props.letrasIniciais}</div>
+            <Link href={'/'} passHref>
+                <div className={`p-4 font-bold ${props.transparente ? 'text-white' : 'text-black'}`}>{props.letrasIniciais}</div>
+            </Link>
             <div className={`flex flex-wrap justify-center items-center w-10 h-9 p-2 mt-2 ${props.menu ? 'rotate-180 ease-out duration-200' : 'rotate-0 ease-out duration-200'}`} onClick={props.onClick}>
                 {props.menu ? (
                     <>
                         <div>
-                            <Image src={props.transparente ? cancel : cancelBlack} alt='Botao cancelar'/>
+                            <Image src={props.transparente ? cancel : cancelBlack} alt='Botao cancelar' />
                         </div>
                     </>
                 ) : (
                     <>
                         <div>
-                            {<Image src={props.transparente ? hamburguer : hamburguerBlack} alt='Botao hamburguer'/>}
+                            {<Image src={props.transparente ? hamburguer : hamburguerBlack} alt='Botao hamburguer' />}
                         </div>
                     </>
                 )}
